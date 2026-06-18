fe01794d964e6d19e992dc40aae8b4e60fa9f778
Guest <guest-cmhud3@lin>
2026-06-18 16:18:11 -0300

    commit

--- 1606/game/src/main.c
+++ 1606/game/src/main.c
@@ -1,29 +1,50 @@
 #include "wasm4.h"
+#include <stdint.h>
 
-const uint8_t smiley[] = {
-    0b11000011,
-    0b10000001,
-    0b00100100,
-    0b00100100,
-    0b00000000,
-    0b00100100,
-    0b10011001,
-    0b11000011,
-};
+// ---------------- FUNÇÕES ÚTEIS ----------------
 
-void start() {
+// desenha pixel (x,y) com cor c
+void pset(int x, int y, int c) {
+  if (x < 0 || x > 159 || y < 0 || y > 159) { return; }
+  if (c == 0) { return; /* transparent */ }
+  int idx = (y*160 + x) >> 2;
+  int shift = (x & 0b11) << 1;
+  int mask = 0b11 << shift;
+  FRAMEBUFFER[idx] = (uint8_t) ((c-1) << shift) | (FRAMEBUFFER[idx] & ~mask);
+}
 
+// retorna cor do pixel (x,y)
+int pget(int x, int y) {
+  if (x < 0 || x > 159 || y < 0 || y > 159) { return 0; }
+  int idx = (y*160 + x) >> 2;
+  int shift = (x & 0b11) << 1;
+  int mask = 0b11 << shift;
+  return ((FRAMEBUFFER[idx] & mask) >> shift) + 1;
+}
+
+uint8_t x, y; // posição do jogador
+
+void start () {
+   x = y = 50; // posição inicial
 }
 
 void update () {
-    *DRAW_COLORS = 2;
-    text("SABOR 67 67 67 67 67 67 67 67", 10, 10);
+  uint8_t dx = 0, dy = 0; // deslocamento, permite mais de uma tecla pressionada
+  uint8_t gamepad = *GAMEPAD1;
+  if (gamepad & BUTTON_LEFT)  { dx = dx - 2; }
+  if (gamepad & BUTTON_RIGHT) { dx = dx + 2; }
+  if (gamepad & BUTTON_UP)    { dy = dy - 2; }
+  if (gamepad & BUTTON_DOWN)  { dy = dy + 2; }
+  x += dx;
+  y += dy;
 
-    uint8_t gamepad = *GAMEPAD1;
-    if (gamepad & BUTTON_1) {
-        *DRAW_COLORS = 4;
-    }
+  *DRAW_COLORS = 2; rect(20, 20, 10, 60); // desenha parede
 
-    blit(smiley, 76, 76, 8, 8, BLIT_1BPP);
-    text("Press X to blink", 16, 90);
+  if (pget(x, y) == 2) { // se colidiu com parede então desfaz movimento
+    x -= dx;
+    y -= dy;
+  }
+
+  *DRAW_COLORS = 3; rect(x, y, 2, 2); // desenha jogador
 }
+
